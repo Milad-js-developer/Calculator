0f540b418cf39da2189bcbec733d955e3992c61c
Milad salmani <miladsalmani.developer@gmail.com>
2022-12-06 00:21:35 +0330

    prints values in input

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const btns = document.querySelectorAll(".btn")
 const calc = document.getElementById("calc");
 const clearAll = document.getElementById("clearAll");
 const clearLast = document.getElementById("clear");
+
+let x;
+
+btns.forEach(item => {
+    item.addEventListener("click", print);
+})
+function print(event) {
+    x = event.target.innerText;
+    if(displayBox.value == 0) {
+        return displayBox.value = x;
+    }
+    return displayBox.value += x;
+}
