3de662977bb49bff8ba624ddff39fb73d4594819
Milad salmani <miladsalmani.developer@gmail.com>
2022-12-06 00:24:19 +0330

    clear latest value in input

--- main.js
+++ main.js
@@ -27,3 +27,12 @@ clearAll.addEventListener("click", allclear);
 function allclear() {
     displayBox.value = 0;
 }
+
+clearLast.addEventListener("click", lastClear);
+function lastClear() {
+    let txt = displayBox.value;
+    displayBox.value = txt.substring(0, txt.length - 1);
+    if(displayBox.value.length == 0) {
+        displayBox.value = 0;
+    }
+}
