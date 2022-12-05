81353f3e5157a304ded6d2d2a419fa7983b22792
Milad salmani <miladsalmani.developer@gmail.com>
2022-12-06 00:18:57 +0330

    select tags in js

---
+++ main.js
@@ -0,0 +1,5 @@
+const displayBox = document.querySelector(".displayBox");
+const btns = document.querySelectorAll(".btn")
+const calc = document.getElementById("calc");
+const clearAll = document.getElementById("clearAll");
+const clearLast = document.getElementById("clear");
